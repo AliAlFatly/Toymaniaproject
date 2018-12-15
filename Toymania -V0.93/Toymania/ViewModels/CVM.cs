b0c74cd626868625281acb51f467be9a1a58bbc0
AliAlFatly <36470971+AliAlFatly@users.noreply.github.com>
2018-12-16 00:36:10 +0100

    V0.93
    Mist, dummy data(failed-> slows everything)
    banner
    user balance by user
    bugfixes
    style-> wordt gedaan door jamie en liam

---
+++ Toymania -V0.93/Toymania/ViewModels/CVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Toymania.Models;
+
+namespace Toymania.ViewModels
+{
+    public class CouponViewModel
+    {
+        public List<Coupon> CL { get; set; }
+    }
+}
---
+++ Toymania -V0.93/Toymania/ViewModels/Toymanager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Toymania.Models;
+
+namespace Toymania.ViewModels
+{
+    public class Toymanager
+    {
+        public List<Toy> t { get; set; }
+        public List<Toy> d { get; set; }
+    }
+}
