88ed4dfae981e04d43edb9c2245779bcc5992e19
LiamFN <36471163+LiamFN@users.noreply.github.com>
2018-12-17 11:44:55 +0100

    v0.94
    xd

---
+++ Toymania -V0.94/Toymania/ViewModels/Recommended.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Toymania.Models;
+
+namespace Toymania.ViewModels
+{
+    public class Recommended
+    {
+        public Toy t { get; set; }
+        public List<Toy> lt { get; set; }
+    }
+}
